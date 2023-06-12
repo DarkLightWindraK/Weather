a9037d5d3fc88497b51041e6a5815113481f2214
Anton Smirnov <DarkLightWindraK@yandex.ru>
2023-06-12 14:38:04 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Антон Смирнов on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
